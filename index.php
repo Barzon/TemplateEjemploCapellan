836a81271f4e5176d84e8c972a36c9bf0372ec75
Alejandro Lira <jlirabarzon@gmail.com>
2015-06-15 17:45:36 -0300

    Update al merged
    Conflicts:
    	index.php

--- index.php
+++ index.php
@@ -31,54 +31,6 @@ include('includes/menu.php'); //definiciones de menu
     </head>
     <body>
         <div class="container-fluid">
-<<<<<<< HEAD
-            <header>
-                <div class="row">
-                    <div class="col-xs-12 bg-warning">                        
-                        <h5 class="title"><strong>Dios, Patria y Familia</strong></h5>                        
-                    </div>
-                    <div class="col-xs-12">                        
-                        <hr>
-                    </div>
-                    <div class="col-xs-12">
-                        <!-- header banner o top banner -->
-                        <div class="col-xs-12"><img src="images/escudo.gif" class="img-thumbnail"/></div>
-                        <div class="col-xs-12"><h3>Colegio Capellan Pascal</h3></div>
-                        <div class="col-xs-12"><p>Un proyecto educacional con formacion valorica integral y de excelencia academica</p></div>                                                
-                    </div>
-                </div>
-                <div class="row">
-                    <!-- menu -->
-                    <?php SystemUtil::createMenuNav($menuParametro);?>
-                </div>
-            </header>
-            <main><!-- contenedor main -->
-                <div class="row">
-                    <div class="col-xs-12 col-sm-2 col-md-3"><!-- columna izquierda -->
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </p>
-                    </div>
-                    
-                    <div class="col-xs-12 col-sm-8 col-md-6"><!-- columna central -->
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </p>
-                        <p>
-                            Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris. Integer in mauris eu nibh euismod gravida. Duis ac tellus et risus vulputate vehicula. Donec lobortis risus a elit. Etiam tempor. Ut ullamcorper, ligula eu tempor congue, eros est euismod turpis, id tincidunt sapien risus a quam. Maecenas fermentum consequat mi. Donec fermentum. Pellentesque malesuada nulla a mi. Duis sapien sem, aliquet nec, commodo eget, consequat quis, neque. Aliquam faucibus, elit ut dictum aliquet, felis nisl adipiscing sapien, sed malesuada diam lacus eget erat. Cras mollis scelerisque nunc. Nullam arcu. Aliquam consequat. Curabitur augue lorem, dapibus quis, laoreet et, pretium ac, nisi. Aenean magna nisl, mollis quis, molestie eu, feugiat in, orci. In hac habitasse platea dictumst.
-                        </p>
-                        <p>
-                            Fusce convallis, mauris imperdiet gravida bibendum, nisl turpis suscipit mauris, sed placerat ipsum urna sed risus. In convallis tellus a mauris. Curabitur non elit ut libero tristique sodales. Mauris a lacus. Donec mattis semper leo. In hac habitasse platea dictumst. Vivamus facilisis diam at odio. Mauris dictum, nisi eget consequat elementum, lacus ligula molestie metus, non feugiat orci magna ac sem. Donec turpis. Donec vitae metus. Morbi tristique neque eu mauris. Quisque gravida ipsum non sapien. Proin turpis lacus, scelerisque vitae, elementum at, lobortis ac, quam. Aliquam dictum eleifend risus. In hac habitasse platea dictumst. Etiam sit amet diam. Suspendisse odio. Suspendisse nunc. In semper bibendum libero.
-                        </p>
-
-                    </div>
-                    <div class="col-xs-12 col-sm-2 col-md-3"><!-- columna derecha -->                    
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </p>
-                    </div>
-                </div>                
-=======
             <?php include('html/header.php'); ?>
             <?php include('html/content/slider.php'); ?>
             <main><!-- contenedor main -->
@@ -91,7 +43,6 @@ include('includes/menu.php'); //definiciones de menu
                     include($content_file);         
                 }
                 ?>
->>>>>>> origin/development
             </main><!-- fin contenedor principal/main -->
             <?php include('html/footer.php'); ?>
         </div>
